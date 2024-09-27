0b367e3445c925bf2e52b6c95976d23a4361a8f2
H-TY <164140214+H-TY@users.noreply.github.com>
2024-09-27 22:55:28 +0000

    Deploying to gh-pages from @ H-TY/Dost_front_Veutify@ea3c1ba5608eb8c62b43510477e058286ac0447e 🚀

--- assets/VContainer-BsDcz-rc.js
+++
@@ -1,1 +0,0 @@
-import{p as n,n as o,v as r,y as l,Z as i,K as c,e as u}from"./index-Bpng_Fgk.js";const m=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),C=l()({name:"VContainer",props:m(),setup(e,a){let{slots:s}=a;const{rtlClasses:t}=i();return c(()=>u(e.tag,{class:["v-container",{"v-container--fluid":e.fluid},t.value,e.class],style:e.style},s)),{}}});export{C as V};
