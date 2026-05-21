828634a31f1dc234ee8f4c5d5a49b98a9e8cff8d
HP_NB <forcom.us@gmail.com>
2026-05-21 15:16:16 +0800

    fix: revert base path configuration in vite.config.mjs for correct routing; update imports for VueRouter

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,7 @@ import Fonts from 'unplugin-fonts/vite';
 import Layouts from 'vite-plugin-vue-layouts';
 import Vue from '@vitejs/plugin-vue';
 import VueRouter from 'unplugin-vue-router/vite';
+import { VueRouterAutoImports } from 'unplugin-vue-router';
 import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 
 // Utilities
@@ -16,8 +17,8 @@ import { generateBreakpointsScss } from './src/styles/setBreakpoints';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	// base: './',
-	base: '/Dost_front_Veutify/',
+	base: './',
+	// base: '/Dost_front_Veutify/',
 	plugins: [
 		VueRouter(),
 		Layouts(),
@@ -44,7 +45,11 @@ export default defineConfig({
 			}
 		}),
 		AutoImport({
-			imports: ['vue', 'vue-router'],
+			imports: [
+				'vue',
+				// 'vue-router',
+				VueRouterAutoImports
+			],
 			eslintrc: {
 				enabled: true
 			},
